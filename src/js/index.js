d1e4d91cca8f8fbdfc354c071fc4b85bd0523dba
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-07-02 23:23:56 +0200

    encaminando las gráficas

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,29 @@
 var datos = [];
 
 var margin = {top: 30, right: 50, bottom: 30, left: 50},
-    width = 900 - margin.left - margin.right,
+    width = 1200 - margin.left - margin.right,
     height = 350 - margin.top - margin.bottom;
+    widthBar = width / 62;
+
+var parseDate = d3.time.format("%d-%b-%y").parse;
+var formatTime = d3.time.format("%e %B");
 
 function loadCSV() {
     d3.csv('temperaturas-prueba.csv', function(err, data) {
         datos = data;
+        datos = data.filter(function(d) { return String(d.fecha).match(/01-08/); });
+        console.log(datos)
+        datos.forEach(function(d) {
+            d.fecha = d.fecha;
+            d.maxima = +d.maxima;
+            d.minima = +d.minima;
+        });
         pintando();
     });
 }
 
 function pintando() {
+
 
     var svg = d3.select('body')
         .append('svg')
@@ -25,17 +37,18 @@ function pintando() {
         .attr("class", "barra")
         .attr("x", 0)
         .attr("y", 0)
-        .attr("width", 20)
+        .attr("width", widthBar)
         .attr("height", 100)
 
+
     .attr("x", function(d, i) {
-            return i * 21 + 30
+            return i * 21 + 300
         })
         .attr("height", function(d) {
-            return d.maxima;
+            return d.maxima * 5;
         })
         .attr("y", function(d) {
-            return height - d.maxima;
+            return height - d.maxima * 5;
         })
 
     svg.selectAll("text")
@@ -46,12 +59,29 @@ function pintando() {
             return d.maxima;
         })
         .attr("x", function(d, i) {
-            return i * 21 + 30;
+            return i * 21 + 300;
         })
         .attr("y", function(d) {
-            return height - d.maxima - 3;
+            return height - d.maxima * 5.1;
         })
         .attr("class", "textoBarras")
+
+
+        svg.selectAll("text.fecha")
+            .data(datos)
+            .enter()
+            .append("text")
+            .text(function(d) {
+                return formatTime(d.fecha);
+            })
+            .attr("x", function(d, i) {
+                return i * 21 + 300;
+            })
+            .attr("y", function(d) {
+                return height + d.maxima;
+            })
+            .attr("class", "textoBarras")
+
 }
 
 
