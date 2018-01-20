2cc3bdcff74f20ea490bb5e081573478e98dba8f
Jorge Aznar <jorgeatgu@gmail.com>
2018-01-20 15:51:45 +0100

    cambios para el resumen de 2017

--- js/resumen.js
+++ js/resumen.js
@@ -1,21 +1,24 @@
-var relativeOffset = anime.timeline();
+var title = document.getElementsByClassName('.resumen-forno');
 
-relativeOffset
-  .add({
-    targets: '#relativeOffset .green',
-    translateX: 250,
-    easing: 'easeOutExpo',
-    offset: 10
-  })
-  .add({
-    targets: '#relativeOffset .blue',
-    opacity: [1,0],
-    easing: 'easeOutExpo',
-    offset: 20 // Starts 600ms before the previous animation ends
-  })
-  .add({
-    targets: '#relativeOffset .red',
-    translateX: 250,
-    easing: 'easeOutExpo',
-    offset: 40 // Starts 800ms before the previous animation ends
-  });
+var combinedFunctionBasedProp = anime({
+    targets: '.resumen-card-rectangle',
+    borderRadius: ['50px', '50px'],
+    width: ['0', '30vw'],
+    height: ['0', '30vw'],
+    duration: 1050,
+    rotate: function(el) {
+        return el.getAttribute('data-rotate');
+    },
+    delay: function(target, index) {
+        // 100ms delay multiplied by every div index, in ascending order
+        return index * 100;
+    },
+    elasticity: function(el, i, l) {
+        return (200 + i * 200);
+    }
+});
+
+
+title.onmouseover = function() {
+    console.log('hola')
+}
