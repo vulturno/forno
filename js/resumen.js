38914dfa8a95e51a4b3849512b89d1c2a23bd930
Jorge Aznar <jorgeatgu@gmail.com>
2018-01-29 00:09:10 +0100

    animacion completada

--- js/resumen.js
+++ js/resumen.js
@@ -1,11 +1,43 @@
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
 $('.ml7 .letters').each(function() {
     $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
+
 var basicTimeline = anime.timeline();
 
-setTimeout(function() {
+
+if (width > 767) {
+    setTimeout(function() {
     basicTimeline
+        .add({
+            targets: '.resumen-card-rectangle',
+            left: function(el) {
+                return el.getAttribute('data-position');
+            },
+            duration: 1050,
+            delay: function(target, index) {
+                return index * 70;
+            },
+            elasticity: function(el, i, l) {
+                return (200 + i * 200);
+            }
+        })
+        .add({
+            targets: '.resumen-card-rectangle',
+            left: ['35vw'],
+            top: ['47.5vh','25vh'],
+            width: ['5vw', '30vw'],
+            height: ['5vw', '30vw'],
+            duration: 1050,
+            delay: function(target, index) {
+                return index * 70;
+            },
+            elasticity: function(el, i, l) {
+                return (200 + i * 200);
+            }
+        })
         .add({
             targets: '.resumen-card-rectangle',
             borderRadius: ['100%', '20%', '50px'],
@@ -35,15 +67,36 @@ setTimeout(function() {
             }
         });
 }, 300);
-
-
-$(document).ready(function() {
-    AOS.init({
-        duration: 600,
-        easing: 'ease-in',
-        disable: function() {
-            var maxWidth = 1024;
-            return window.innerWidth < maxWidth;
-        }
-    });
-});
+} else {
+    setTimeout(function() {
+        basicTimeline
+            .add({
+                targets: '.resumen-card-rectangle',
+                borderRadius: ['100%', '20%', '50px'],
+                duration: 1050,
+                rotate: function(el) {
+                    return el.getAttribute('data-rotate') * 7;
+                },
+                delay: function(target, index) {
+                    return index * 70;
+                },
+                elasticity: function(el, i, l) {
+                    return (200 + i * 200);
+                }
+            })
+            //Letter effect by: http://tobiasahlin.com/moving-letters/#
+            .add({
+                targets: '.letter',
+                opacity: 1,
+                translateY: ["1.1em", 0],
+                translateX: ["0.55em", 0],
+                translateZ: 0,
+                rotateZ: [180, 0],
+                duration: 750,
+                easing: "easeOutExpo",
+                delay: function(el, i) {
+                    return 50 * i;
+                }
+            });
+    }, 300);
+}
