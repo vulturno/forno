752c73e24021a2b5b6bd3065ffb120c9450306f5
Jorge Aznar <jorgeatgu@gmail.com>
2018-04-17 22:26:08 +0200

    ajustando lso ejes

--- src/js/lluvias.js
+++ src/js/lluvias.js
@@ -1,59 +1,91 @@
 function lluviaMes() {
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 1200 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    // set the ranges
-    var x = d3.scaleBand()
-              .range([0, width])
-              .padding(0.1);
-    var y = d3.scaleLinear()
-              .range([height, 0]);
+    widthBar = width / 76;
 
-    // append the svg object to the body of the page
-    // append a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
+    var x = d3.scaleBand()
+        .range([0, width]);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    var xAxis = d3.axisBottom()
+        .scale(x)
+        .tickPadding(5)
+        .tickFormat(function(d) { return d })
+        .ticks(10);
+
+    var yAxis = d3.axisLeft()
+        .scale(y)
+        .tickPadding(10)
+        .tickFormat(function(d) { return d })
+        .tickSize(-width + 30)
+        .ticks(16);
+
     var svg = d3.select('.lluvias-por-mes-chart-container')
         .append('svg')
         .attr('class', 'lluvias-mes-media-chart-graph')
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-      .append("g")
+        .append("g")
         .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    // get the data
     d3.csv("csv/meses-lluvia.csv", function(error, data) {
-      if (error) throw error;
+        if (error) throw error;
 
-      // format the data
-      data.forEach(function(d) {
-        d.cantidad = +d.cantidad;
-      });
+        data = data.filter(function(d) {
+            return String(d.mes).match(/APR/);
+        });
 
-      // Scale the range of the data in the domains
-      x.domain(data.map(function(d) { return d.mes; }));
-      y.domain([0, d3.max(data, function(d) { return d.cantidad + (d.cantidad / 4) })]);
+        data.forEach(function(d) {
+            d.cantidad = +d.cantidad;
+        });
 
-      // append the rectangles for the bar chart
-      svg.selectAll(".bar")
-          .data(data)
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("x", function(d) { return x(d.mes); })
-          .attr("width", x.bandwidth())
-          .attr("y", function(d) { return y(d.cantidad); })
-          .attr("height", function(d) { return height - y(d.cantidad); });
+        var nest = d3.nest()
+            .key(function(d) {
+                return d.mes;
+            })
+            .entries(data)
 
-      // add the x Axis
-      svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
 
-      // add the y Axis
-      svg.append("g")
-          .call(d3.axisLeft(y));
+
+        var mesMenu = d3.select("#mes")
+
+        mesMenu
+            .append("select")
+            .selectAll("option")
+            .data(nest)
+            .enter()
+            .append("option")
+            .attr("value", function(d) {
+                return d.key;
+            })
+            .text(function(d) {
+                return d.key;
+            })
+
+        x.domain(data.map(function(d) { return d.fecha; }));
+        y.domain([0, d3.max(data, function(d) { return d.cantidad + (d.cantidad / 4) })]);
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.fecha); })
+            .attr("width", widthBar)
+            .attr("y", function(d) { return y(d.cantidad); })
+            .attr("height", function(d) { return height - y(d.cantidad); });
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .call(yAxis);
+
 
     });
 
