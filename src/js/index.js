fa97a6c88f3f14dc2d9d82aee24f8a7bc5f1c3e7
Jorge Aznar <jorgeatgu@gmail.com>
2018-12-21 15:32:29 +0100

    start reforno

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,3 @@
-function menu() {
-    var overlay = document.querySelector('.overlay');
-    var navigation = document.querySelector('.navegacion');
-    var body = document.querySelector('body');
-    var elementBtn = document.querySelectorAll('.navegacion-btn');
-    var burger = document.querySelector('.burger');
-
-    function classToggle() {
-        burger.classList.toggle('clicked');
-        overlay.classList.toggle('show');
-        navigation.classList.toggle('show');
-        body.classList.toggle('overflow');
-    }
-
-    document.querySelector('.burger').addEventListener('click', classToggle);
-    document.querySelector('.overlay').addEventListener('click', classToggle);
-
-    for(i=0; i<elementBtn.length; i++){
-        elementBtn[i].addEventListener("click", function(){
-            removeClass();
-        });
-    }
-
-    function removeClass() {
-        overlay.classList.remove("show");
-        navigation.classList.remove("show");
-        burger.classList.remove("clicked");
-
-    }
-}
-
-menu();
-
 var dataFiltered;
 var xRange;
 var yRange;
@@ -38,9 +5,6 @@ var xAxis;
 var yAxis;
 var temp = "ºC";
 var barPadding = 2;
-
-var dedicatoria = 'Dedicado a Maria del Carmen Tobajas Urieta y Agustín Aznar Gracia. Gracias por todo.';
-    console.log(dedicatoria);
 
 var margin = { top: 50, right: 50, bottom: 50, left: 110 },
     width = 1200 - margin.left - margin.right,
---
+++ src/js/v2.js
@@ -0,0 +1,35 @@
+const dedicatoria = 'Dedicado a Maria del Carmen Tobajas Urieta y Agustín Aznar Gracia. Gracias por todo.';
+    console.log(dedicatoria);
+
+function menu() {
+    var overlay = document.querySelector('.overlay');
+    var navigation = document.querySelector('.navegacion');
+    var body = document.querySelector('body');
+    var elementBtn = document.querySelectorAll('.navegacion-btn');
+    var burger = document.querySelector('.burger');
+
+    function classToggle() {
+        burger.classList.toggle('clicked');
+        overlay.classList.toggle('show');
+        navigation.classList.toggle('show');
+        body.classList.toggle('overflow');
+    }
+
+    document.querySelector('.burger').addEventListener('click', classToggle);
+    document.querySelector('.overlay').addEventListener('click', classToggle);
+
+    for(i=0; i<elementBtn.length; i++){
+        elementBtn[i].addEventListener("click", function(){
+            removeClass();
+        });
+    }
+
+    function removeClass() {
+        overlay.classList.remove("show");
+        navigation.classList.remove("show");
+        burger.classList.remove("clicked");
+
+    }
+}
+
+menu();
